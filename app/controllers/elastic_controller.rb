c8bb210dd4153cee47935720b387856999fec75b
GBardis <crisolivasavatage@hotmail.com>
2017-05-25 21:21:14 +0300

    elastisearch installation and connect to remote server

---
+++ app/controllers/elastic_controller.rb
@@ -0,0 +1,21 @@
+class ElasticController < ApplicationController
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
+  def elastic
+    client = Elasticsearch::Model.client
+    client = Elasticsearch::Client.new url: 'https://kteam:draewjUgbksIjcv19epwhpkcpnzieqkn@159.8.53.13:443',
+    transport_options: {
+      ssl: {
+        ca_file: 'public/elasticsearch_cert.pem',
+      }
+    }
+
+    @response = client.search index:'threatdb_2017.05.19',body:{"query":{"bool":{"must":{"query_string":{"default_field":"threatdb_tri.threat_type","query":"t_spamip"}},"must_not":[],"should":[]}},"from":0,"size":1,"sort":[],"aggs":{}}
+
+    #@result =JSON.parse(JSON.dump(@response.as_json) , object_class:OpenStruct)
+    @result = JSON.parse(JSON.dump(@response.as_json))
+    @obj = Hashie::Mash.new @result
+    #byebug
+  end
+end
---
+++ test/controllers/elastic_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ElasticControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
