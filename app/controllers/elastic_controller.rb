2b1aa2d9b59c2048102dccaf3cd19f507e4be7d2
GBardis <crisolivasavatage@hotmail.com>
2017-06-01 22:12:06 +0300

    devise install

--- app/controllers/elastic_controller.rb
+++ app/controllers/elastic_controller.rb
@@ -13,9 +13,20 @@ class ElasticController < ApplicationController
 
     #  @response = client.search index:'threatdb_2017.05.26',body:{"query":{"bool":{"must":{"query_string":{"default_field":"threatdb_tri.threat_type","query":"t_spamip"}},"must_not":[],"should":[]}},"from":0,"size":1,"sort":[],"aggs":{}}
 
-    @response = client.search index:'threatdb_2017.05.26',body:{"query":{"bool":{"must":[{"match_all":{}}],"must_not":[],"should":[]}},"from":0,"size":10,"sort":[],"aggs":{}}
+    @response = client.search index:'threatdb_2017.05.26',body:{"query":{"bool":{"must":[{"match_all":{}}],"must_not":[],"should":[]}},"from":0,"size":100,"sort":[],"aggs":{}}
     @result = JSON.parse(JSON.dump(@response.as_json))
     @obj = Hashie::Mash.new @result
-    #byebug
+  end
+  def elastic_second
+    client = Elasticsearch::Model.client
+    client = Elasticsearch::Client.new url: 'https://kteam:draewjUgbksIjcv19epwhpkcpnzieqkn@159.8.53.13:443',
+    transport_options: {
+      ssl: {
+        ca_file: 'public/elasticsearch_cert.pem',
+      }
+    }
+    @threatdb_tri = client.search index:'threatdb_2017.05.26',body:{"query":{"bool":{"must":[{"match_all":{}}],"must_not":[],"should":[]}},"from":0,"size":10,"sort":[{ "threat_tri":{"order":"desc"}}],"aggs":{}}
+    @result_second = JSON.parse(JSON.dump(@threatdb_tri.as_json))
+    @obj_second = Hashie::Mash.new @result_second
   end
 end
