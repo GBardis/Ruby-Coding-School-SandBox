81d1af5d8fe6576b0819fe183b965c3e8642c552
spyratosa <spyratosa@gmail.com>
2017-06-19 23:28:59 +0300

    refactored project files and routes

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -63,8 +63,9 @@ class ApiController < ApplicationController
     return @@client.search index: @@index_name, body: body
   end
 
-  # def analytics
-  #    @somevar =[1,2,3,4,5]
-  #    render('analytics/show')
-  # end
+  def analytics
+      @search = Search.search(query:{match_all:{}},size:10,sort:['threat_tri': { "order": "desc" } ])
+      @search_country = Search.search(size:0,aggs:{'group_by_country':{'terms':{'field':'country.raw'}}})
+      @search_by_tri = Search.search(size:0,aggs:{'group_by_country':{'terms':{'field':'country.raw','order':{'average_threat_tri':'desc'}},aggs:{'average_threat_tri':{avg:{'field':'threat_tri'}}}}})
+  end
 end
